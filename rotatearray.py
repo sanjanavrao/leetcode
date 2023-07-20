58a7b3e1e2a3b715a376ee443e4958a13428a0e2
sanjanavrao <92152313+sanjanavrao@users.noreply.github.com>
2023-07-20 20:28:17 +0530

    rotate the array k times

---
+++ rotatearray.py
@@ -0,0 +1,29 @@
+class Solution(object):
+
+    def reverse(self, nums, start, end):
+        """
+        :type nums: List[int]
+        :type start: int
+        :type end: int
+        :rtype: None
+        """
+        while start < end: #
+            temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp 
+            start += 1
+            end -= 1
+
+
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        self.reverse(nums,0,len(nums)-1)
+        self.reverse(nums,0, k-1)
+        self.reverse(nums,k, len(nums)-1)
+
+    
